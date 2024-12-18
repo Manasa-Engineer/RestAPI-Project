cdbd1bbe2d7285d165d5baf137aca47cf552d867
Manasa-Engineer <manasa.lakshmi2208@gmail.com>
2024-12-18 10:42:46 -0700

    Update ClientRepo.java

--- RestCallHandler/src/main/java/com/example/restcallhandler/Dao/ClientRepo.java
+++ RestCallHandler/src/main/java/com/example/restcallhandler/Dao/ClientRepo.java
@@ -2,6 +2,6 @@ package com.example.restcallhandler.Dao;
 
 import com.example.restcallhandler.Pojo.Subscriber;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+@Repository
 public interface ClientRepo extends JpaRepository<Subscriber, Integer> {
 }
